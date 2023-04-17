ecba39848f29260c3c217aba72e6511285a16be7
pinkpony1106 <juliaegorova221098@gmail.com>
2023-04-17 22:08:12 +0300

    done extended repeater fix

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,36 +15,33 @@ const { NotImplementedError } = require("../extensions/index.js");
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-// let str = "STRING";
-// let options = {
-//   repeatTimes: 3,
-// };
 
 function repeater(str, options) {
   let result = [];
-
+  let string = typeof str === "string" ? str : String(str);
   if (
     options.hasOwnProperty("addition") ||
     options.hasOwnProperty("additionRepeatTimes") ||
     options.hasOwnProperty("additionSeparator")
   ) {
     let addit = new Array(options.additionRepeatTimes)
-      .fill(options.addition)
-      .join(options.additionSeparator ? options.additionSeparator : "");
-    let string = str + addit;
+      .fill(
+        typeof options.addition === "string"
+          ? options.addition
+          : String(options.addition)
+      )
+      .join(options.additionSeparator ? options.additionSeparator : "|");
+    string += addit;
     result = new Array(options.repeatTimes)
       .fill(string)
       .join(options.separator ? options.separator : "+");
   } else {
-    let string = str;
     result = new Array(options.repeatTimes)
       .fill(string)
       .join(options.separator ? options.separator : "+");
   }
   return result;
 }
-
-// console.log(repeater(str, options));
 
 module.exports = {
   repeater,
